4454cf8de690f4592a35616a76606880bd737fb5
frank <frank.hillard@gmail.com>
2017-12-22 13:24:16 +0100

    add package json: node_modulesq web3 are required for truffle deployment script

--- README.md
+++ README.md
@@ -1,20 +1,26 @@
 # donation
 Solidity smart contract that facilitates cryptomoney donations from philantrophists to people in need
 
-# installation instructions
+## installation instructions
 
-// create new account 
+### create new account geth 
+```
 geth --datadir="~/dev/donation/data" account new
-// intialize genesis for testnet
+```
+### intialize genesis for testnet
+```
 geth --datadir="~/dev/donation/data" init "customGENESIS.json"
-
-// launch geth with testnet
+```
+### launch geth with testnet
+```
 geth --datadir "~/dev/donation/data" --identity "Participant1" --networkid "41514" --nodiscover --unlock "0" --port "30303"  --rpc --rpcaddr "localhost" --rpcport "8545" --rpcapi "admin,eth,miner,net,web3" --rpccorsdomain "*" --mine --minerthreads 1 --autodag --nat "any" --maxpeers "2" 
-
-// compile contracts
+```
+### compile contracts
+```
 cd solidity
 truffle compile -all
-
-// deploy contracts
+```
+### deploy contracts
+```
 truffle migrate --reset
-
+```
---
+++ solidity/migrations/2_deploy_contracts.js
@@ -0,0 +1,53 @@
+var q = require('../../node_modules/q');
+var fs = require('../../node_modules/fs-extra/lib');
+var Web3 = require('../../node_modules/web3');
+var truffleConfig = require('../../solidity/truffle.json');
+
+var DONATION = require('../../solidity/build/contracts/Donation.sol.js');
+
+
+module.exports = function(deployer) {
+   var ADDR_DEPLOYER = truffleConfig.donation.addr_deployer;
+   
+   var adddonation;
+
+   var donation;
+
+    var deployDonation = function() {
+        var def = q.defer();
+        deployer.deploy(Donation).then(function() {
+            adddonation = Donation.address;
+            donation = Donation.at(adddonation);
+            console.log('  >> Donation deployed at address ', adddonation);
+            def.resolve();
+        }, function(err) {
+            def.reject(err);
+        });
+        return def.promise;
+    };
+    
+    var writeDeployConfigFile = function() {
+        var def = q.defer();
+        // Once everything is deployed, creates a file with contract addresses
+        if (fs != undefined) {
+            fs.writeFileSync(
+                __dirname + '/../../server/config/deploy-config.json', 
+                JSON.stringify({
+                   'DONATION': adddonation,
+                   'DONATION_abi': DONATION.all_networks.default.abi,
+                }),
+                'utf-8'
+            );
+            def.resolve();
+        } else {
+            def.reject('fs is undefined');
+        }
+        return def.promise;
+    };
+
+    deployDonation()
+    .then(writeDeployConfigFile)
+    .catch(function(err) {
+        console.log('  >> ' + err);
+    });
+};
