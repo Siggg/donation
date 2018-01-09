0f6111873802520988817e60527f732cd1a0a8dd
frank <frank.hillard@gmail.com>
2018-01-09 19:31:11 +0100

    Add Unit test: Register beneficiary (async) verify number of beneficiary in the end

--- solidity/test/test.js
+++ solidity/test/test.js
@@ -21,6 +21,7 @@ var watchTransaction = function(txhash, callback) {
 		          	var found = false;
 		          	for (var i = 0; i < block.transactions.length; i++) {
 						if (block.transactions[i].hash == txhash) {
+							//console.log("tx desc", block.transactions[i]);
 							found = true;
 							break;
 						}
@@ -45,36 +46,60 @@ var watchTransaction = function(txhash, callback) {
 
 var don;
 contract("Donation", function(accounts) {
-	it("check number of beneficiaries", function() {
+	it("check number of beneficiaries at initial state", function() {
 		return Donation.deployed().then(function(instance){
 			don = instance;
 			return	don.getBeneficiaryCount.call({from: account_deployer});
-		}).then(function(count){ console.log("number beneficiaries:", count)});	
+		}).then(function(count){ 
+			console.log("number beneficiaries:", count.toNumber());
+			assert.equal(count, 0, "beneficiaries count should be 0");});	
 	});
 
-	it("check beneficiaries", function() {
+	it("check beneficiaries at initial state", function() {
 		return Donation.deployed().then(function(instance){
 			don = instance;
-			return	instance["getPaginateBeneficiaries"].call(0, 10, {from: account_deployer});
-		}).then(function(beneficiaries){ console.log("beneficiaries(0,10):", beneficiaries)});	
+			return	instance["getPaginateBeneficiaries"].call(0, 99, {from: account_deployer});
+		}).then(function(beneficiaries){ 
+			//console.log("beneficiaries(0,99):", beneficiaries);
+			for (var i in beneficiaries) {
+				if (beneficiaries[i] == 0x0) { //0x0000000000000000000000000000000000000000
+				}
+				else {
+					console.log("beneficiary", i, beneficiaries[i]);
+				}
+			}
+		});	
 	});
 
 	it("register first account", function() {
 		return Donation.deployed().then(function(instance){		
 			don = instance;
-			return instance["registerBeneficiary"].sendTransaction(account_certifier, { "gas": 30000, "from": account_deployer});
+			console.log("Attempt registerBeneficiary", account_certifier);
+			return instance["registerBeneficiary"].sendTransaction(account_certifier, { "gas": 50000, "from": account_deployer});
 			//return instance.registerBeneficiary(account_certifier, {from: account_deployer});
 		}).then(function(tx){
-			console.log("Attempt registerBeneficiary", tx);
-			watchTransaction(tx, function(err, result){ 
-				if (err) 
-					console.error("failed watching tx", tx); 
-				else {
-					if (result == true) {
-						console.error("tx DONE", tx); 
+			console.log("Waiting confirmation of", tx, "...");
+
+			return new Promise(function(resolve,reject) {
+				watchTransaction(tx, function(err, result){ 
+					if (err) {
+						console.error("failed watching tx", tx); 
+						reject();
 					}
-				}
+					else {
+						if (result == true) {
+							console.log("TX DONE", tx); 
+						}
+						resolve();
+					}
+				});
+
 			});
+		}).then(function(){
+			return	don.getBeneficiaryCount.call({"from": account_deployer});
+		}).then(function(count){ 
+			console.log("number beneficiaries:", count.toNumber());
+			assert.equal(count.toNumber(),1, "beneficiaries count should be 1");
 		});
 
 	});
